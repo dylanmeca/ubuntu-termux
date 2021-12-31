11404039e38051fbfb56de05413d3851f65f7f45
Dylan Meca <55631040+dylanmeca@users.noreply.github.com>
2021-12-30 21:21:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 tarball="ubuntu-rootfs.tar.gz"
 if [ "$first" != 1 ];then
 	if [ ! -f $tarball ]; then
-		printf '\e[1;34m%-6s\e[m' '[*] Download Rootfs, this may take a while base on your internet speed'
+		printf '\n\e[1;34m%-6s\e[m' '[*] Download Rootfs, this may take a while base on your internet speed'
 		case `dpkg --print-architecture` in
 		aarch64)
 			archurl="arm64" ;;
@@ -20,20 +20,20 @@ if [ "$first" != 1 ];then
 		x86_64)
 			archurl="amd64" ;;	
 		*)
-			echo -e "\e[1;34m [*] Unknown Architecture"; exit 1 ;;
+			echo -e "\e[1;31m [*] Unknown Architecture"; exit 1 ;;
 		esac
 		wget "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.3-base-${archurl}.tar.gz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
 	cd "$folder"
-	printf '\e[1;34m%-6s\e[m' '[*] Decompressing Rootfs, please be patient.'
+	printf '\n\e[1;34m%-6s\e[m' '[*] Decompressing Rootfs, please be patient.'
 	proot --link2symlink tar -xf ${cur}/${tarball}||:
 	cd "$cur"
 fi
 mkdir -p ubuntu-binds
 bin=ubuntu
-printf '\e[1;34m%-6s\e[m' '[*] Configuring Ubuntu...'
+printf '\n\e[1;34m%-6s\e[m' '[*] Configuring Ubuntu...'
 cat > $bin <<- EOM
 #!/bin/bash
 cd \$(dirname \$0)
